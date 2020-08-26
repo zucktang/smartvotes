52d72606acf9f08787e653a752871efc90253e8f
Nontaphat Noijai <zucktang@gmail.com>
2020-08-26 17:42:11 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/su/cp/smartvotes/MainActivity.java
@@ -0,0 +1,15 @@
+package th.ac.su.cp.smartvotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
